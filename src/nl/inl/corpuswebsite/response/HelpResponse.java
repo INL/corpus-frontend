ae633cc6e1167c6adfaff6eb81bae9c3cee32773
niestadt <jan.niestadt@inl.nl>
2015-06-12 11:05:59 +0200

    Differentiated where help finds its css for AutoSearch vs. the rest.

--- src/nl/inl/corpuswebsite/response/HelpResponse.java
+++ src/nl/inl/corpuswebsite/response/HelpResponse.java
@@ -3,9 +3,9 @@
  */
 package nl.inl.corpuswebsite.response;
 
-import org.apache.velocity.VelocityContext;
-
 import nl.inl.corpuswebsite.BaseResponse;
+
+import org.apache.velocity.VelocityContext;
 
 /** Show help page. */
 public class HelpResponse extends BaseResponse {
@@ -15,8 +15,8 @@ public class HelpResponse extends BaseResponse {
 		putFileContentIntoContext("content", servlet.getHelpPage(corpus));
 		
 		VelocityContext context = getContext();
-		String pathToTop = ".";
-		context.put("pathToTop", pathToTop); // correct for most pages, but for "list of corpora" it's "."
+		String pathToTop = corpus.equals("autosearch") ? "." : "..";
+		context.put("pathToTop", pathToTop);
 		context.put("brandLink", corpus.equals("autosearch") ? pathToTop : "search");
 
 		displayHtmlTemplate(servlet.getTemplate("contentpage"));

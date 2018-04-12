a5e6fe5f20ff4637ce480a44483a1c68e5a551f0
Koen Mertens <koen.mertens@ivdnt.org>
2018-04-12 09:46:52 +0200

    Show spinner while exporting csv. Fixes #65

--- src/main/webapp/js/singlepage-interface.js
+++ src/main/webapp/js/singlepage-interface.js
@@ -649,7 +649,10 @@ SINGLEPAGE.INTERFACE = (function() {
 			console.log('CSV download url', url, blsParam);
 		}
 
-		$button.addClass('disabled').attr('disabled', true);
+		$button
+			.addClass('disabled')
+			.attr('disabled', true)
+			.prepend('<span class="fa fa-spinner fa-spin"></span>');
 		$.ajax(url, {
 			accepts: 'application/csv',
 			cache: 'false',
@@ -662,7 +665,10 @@ SINGLEPAGE.INTERFACE = (function() {
 				saveAs(b, 'data.csv'); // FileSaver.js
 			},
 			complete: function() {
-				$button.removeClass('disabled').attr('disabled', false);
+				$button
+					.removeClass('disabled')
+					.attr('disabled', false)
+					.find('.fa-spinner').remove();
 			}
 		});
 	}

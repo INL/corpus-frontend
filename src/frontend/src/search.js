51783d5fe8e88eb7349116014fb8829f4451158c
Koen Mertens <koen.mertens@ivdnt.org>
2018-08-23 13:33:13 +0200

    Make webpack output 3 different files as they're used in the pages

---
+++ src/frontend/src/search.js
@@ -0,0 +1,7 @@
+import * as singlepage from './modules/singlepage';
+import * as singlepage_bls from './modules/singlepage-bls';
+import * as singlepage_form from './modules/singlepage-bls';
+import * as singlepage_interface from './modules/singlepage-interface';
+import * as singlepage_cqlparser from './modules/singlepage-cqlparser';
+import * as singlepage_querybuilder from './modules/cql_querybuilder';
+import * as tutorial from './modules/tutorial';
--- src/frontend/webpack.config.js
+++ src/frontend/webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        article: './src/article.js',
+        corpora: './src/corpora.js',
+        search: './src/search.js',
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
     },
 };

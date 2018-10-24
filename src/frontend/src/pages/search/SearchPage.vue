f8387d56c1f59a35cfd5a0da059be52bd32b40d9
Koen Mertens <koen.mertens@ivdnt.org>
2018-10-24 10:46:16 +0200

    Clean up html a bit

---
+++ src/frontend/src/pages/search/SearchPage.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<SearchForm class="row contentbox" id="mainForm"/>
+		<QuerySummary v-if="viewedResults" class="row contentbox" id="summary"/>
+		<Results v-show="viewedResults" class="row contentbox" id="results"/>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import $ from 'jquery';
+
+import * as RootStore from '@/store';
+
+import SearchForm from '@/pages/search/form/SearchForm.vue';
+import QuerySummary from '@/pages/search/QuerySummary.vue';
+import Results from '@/pages/search/Results.vue';
+
+
+export default Vue.extend({
+	components: {
+		SearchForm,
+		QuerySummary,
+		Results
+	},
+	computed: {
+		viewedResults: RootStore.get.viewedResults
+	},
+
+	mounted() {
+		$(document).trigger('vue-root-mounted');
+	}
+})
+
+</script>
+
+<style lang="scss">
+
+</style>
--- src/frontend/src/pages/search/form/SearchForm.vue
+++ src/frontend/src/pages/search/form/SearchForm.vue
@@ -1,28 +1,22 @@
 <template>
-	<form class="col-xs-12 contentbox" id="mainForm"> <!-- TODO id -->
-		<div class="row">
+	<form>
+		<Annotations class="col-xs-12 col-md-6" id="searchcontainer"/>
+		<Filters class="col-xs-12 col-md-6" id="filtercontainer"/>
 
-			<Annotations class="col-xs-12 col-md-6" id="searchcontainer"/> <!-- todo id -->
-			<Filters class="col-xs-12 col-md-6" id="filtercontainer"/>
-
-			<div class="col-xs-12">
-				<hr>
-				<div style="display:inline-block; position: absolute; bottom: 0;">
-					<button type="submit" class="btn btn-primary btn-lg">Search</button>
-					<button type="reset" class="btn btn-default btn-lg" title="Start a new search">Reset</button>
-				</div>
-
-				<button type="button" class="btn btn-lg btn-default pull-right" data-toggle="modal" data-target="#settingsModal"><span class="glyphicon glyphicon-cog" style="vertical-align:text-top;"></span></button>
+		<div class="col-xs-12">
+			<hr>
+			<div style="display:inline-block; position: absolute; bottom: 0;">
+				<button type="submit" class="btn btn-primary btn-lg">Search</button>
+				<button type="reset" class="btn btn-default btn-lg" title="Start a new search">Reset</button>
 			</div>
-		</div>
 
+			<button type="button" class="btn btn-lg btn-default pull-right" data-toggle="modal" data-target="#settingsModal"><span class="glyphicon glyphicon-cog" style="vertical-align:text-top;"></span></button>
+		</div>
 	</form>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-
-import $ from 'jquery';
 
 import Annotations from '@/pages/search/form/Annotations.vue';
 import Filters from '@/pages/search/form/Filters.vue';
@@ -34,10 +28,6 @@ export default Vue.extend({
 	},
 	props: {
 		afterMount: Object as any
-	},
-	mounted() {
-		console.log('inside searchform component mounted')
-		$(document).trigger('form-mounted');
 	}
 })
 

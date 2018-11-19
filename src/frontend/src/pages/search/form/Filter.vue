44ec85d77a9c8c271a64aaed986ac9140eb8831f
Koen Mertens <koen.mertens@ivdnt.org>
2018-11-19 13:33:56 +0100

    Fix filters causing vuex warnings

--- src/frontend/src/pages/search/form/Filter.vue
+++ src/frontend/src/pages/search/form/Filter.vue
@@ -18,16 +18,16 @@
 					:name="inputId"
 					:options="options"
 
-					v-model.lazy="value"
+					v-model.lazy="valueAdapterMultipleArray"
 				/>
 			</div>
 		</template>
 		<template v-else-if="uiType === 'range'">
 			<div class="col-xs-4">
-				<input type="text" :id="inputId+'_lower'" placeholder="From" class="form-control" autocomplete="off" v-model="value[0]">
+				<input type="text" :id="inputId+'_lower'" placeholder="From" class="form-control" autocomplete="off" v-model.lazy="valueAdapterRange.low">
 			</div>
 			<div class="col-xs-4">
-				<input type="text" :id="inputId+'_upper'" placeholder="To" class="form-control" autocomplete="off" v-model="value[1]">
+				<input type="text" :id="inputId+'_upper'" placeholder="To" class="form-control" autocomplete="off" v-model.lazy="valueAdapterRange.high">
 			</div>
 		</template>
 		<template v-else-if="uiType === 'checkbox'">
@@ -36,11 +36,12 @@
 					<!-- TODO optimize this, currently rewriting all values, ergo rerendering all checkboxes every time one changes -->
 					<label :for="inputId+'_'+index"><input
 						type="checkbox"
-						:checked="value.includes(option.value)"
+
 						:value="option.value"
 						:name="inputId+'_'+index"
 						:id="inputId+'_'+index"
-						@change="value = $event.target.checked ? value.concat([option.value]) : value.filter(v => v !== option.value)"
+
+						v-model="valueAdapterMultipleMap[option.value]"
 
 					> {{option.label || option.value}}</label>
 				</div>
@@ -49,16 +50,15 @@
 		<template v-else-if="uiType === 'radio'">
 			<div class="col-xs-12">
 				 <div class="radio" v-for="(option, index) in options" :key="index">
-					<!-- TODO optimize this, currently rewriting all values, ergo rerendering all checkboxes every time one changes -->
 					<label :for="inputId+'_'+index"><input
 						type="radio"
 						:value="option.value"
 						:name="inputId"
 						:id="inputId+'_'+index"
 
-						v-model="value[0]"
-						@click="$event.target.checked ? value = [''] : undefined /* clear if clicked again */"
-						@input.space="$event.target.checked ? value = [''] : undefined /* clear if clicked again */"
+						v-model="valueAdapterSingle"
+						@click="$event.target.checked ? valueAdapterSingle = '' : undefined /* clear if clicked again */"
+						@input.space="$event.target.checked ? valueAdapterSingle = '' : undefined /* clear if clicked again */"
 					> {{option.label || option.value}}</label>
 				</div>
 			</div>
@@ -73,9 +73,8 @@
 					:placeholder="displayName"
 					:autocomplete="autocomplete"
 
-
 					ref="autocomplete"
-					v-model.lazy="value[0]"
+					v-model.lazy="valueAdapterSingle"
 				/>
 			</div>
 		</template>
@@ -103,6 +102,9 @@ export default Vue.extend({
 	props: {
 		filter: Object as () => CorpusStore.NormalizedMetadataField,
 	},
+	data: () => ({
+		valueAdapterMultipleMap: {} as { [value: string]: boolean, }
+	}),
 	computed: {
 		id(): string { return this.filter.id },
 		displayName(): string { return this.filter.displayName },
@@ -115,21 +117,74 @@ export default Vue.extend({
 		autocomplete(): boolean { return this.filter.uiType === 'combobox'; },
 		autocompleteUrl(): string { return `${BLS_URL}/autocomplete/${this.filter.id}`},
 
+		// we need some adapters to translate from string[] -> string
+		// or from string[] -> { key: boolean }
+		// and vice-versa
+		// or we get errors where we mutate inside arrays in the store directly etc
 
-		value: {
-			get(): string[] {
-				return FormStore.get.metadataValue(this.id).values;
-			},
-			set(values: string[]): void {
+		// text, radio, combobox
+		valueAdapterSingle: {
+			get(): string { return FormStore.get.metadataValue(this.id).values[0] || ''; },
+			set(v: string): void {
 				return FormStore.actions.filter({
 					id: this.id,
-					values
+					values: [v]
 				});
 			}
-		}
+		},
+
+		valueAdapterRange(): {low: string; high: string;} {
+			const id = this.id;
+			const storeValue = FormStore.get.metadataValue(id).values;
+
+			const adapter = {
+				get low(): string { return storeValue[0] || '' },
+				set low(v: string) { FormStore.actions.filter({
+					id,
+					values: [v, adapter.high]
+				});},
+
+				get high(): string { return storeValue[1] || ''},
+				set high(v: string) { FormStore.actions.filter({
+					id,
+					values: [adapter.low, v]
+				});},
+			};
+			return adapter;
+		},
+
+		valueAdapterMultipleArray: {
+			get(): string[] { return FormStore.get.metadataValue(this.id).values; },
+			set(v: string[]) { FormStore.actions.filter({
+				id: this.id,
+				values: v
+			});}
+		},
 	},
 	methods: {
-		autocompleteSelected(value: string) { this.value = [value]; }
+		autocompleteSelected(value: string) { this.valueAdapterSingle = value; }
+	},
+	watch: {
+		options: {
+			immediate: true,
+			handler(options: Array<{value: string, label: string}>) {
+				const newValues = {} as {[key:string]: boolean};
+				options.forEach(opt => {
+					newValues[opt.value] = this.valueAdapterMultipleArray.includes(opt.value)
+				});
+				this.valueAdapterMultipleMap = newValues;
+			}
+		},
+		valueAdapterMultipleMap: {
+			immediate: true,
+			deep: true,
+			handler(v: {[key: string]: boolean}) {
+				FormStore.actions.filter({
+					id: this.id,
+					values: Object.entries(v).filter(([option, checked]) => checked).map(([option]) => option)
+				})
+			}
+		}
 	}
 });
 </script>

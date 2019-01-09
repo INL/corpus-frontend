00dae5f192f4c9884fe17de2383d3089f194f3af
Koen Mertens <koen.mertens@ivdnt.org>
2019-01-09 11:57:31 +0100

    Corpora page: Fix user formats being listed as built in formats
    - when uploading config files, add .blf.yaml/blf.json extension instead of
    direct yaml/json for parity with other formats.

--- src/frontend/src/corpora.ts
+++ src/frontend/src/corpora.ts
@@ -172,7 +172,7 @@ createHandler({selector: 'select[data-autoupdate="format"]', event: DataEvent.FO
 	this
 		.html(Mustache.render(template, {
 			userName: serverInfo.user.id,
-			builtinFormats: newFormats.filter(f => !f.owner && f.isVisible == null /* temporary, for when bls does not support the property yet */ || f.isVisible),
+			builtinFormats: newFormats.filter(f => !f.owner && (f.isVisible == null /* temporary, for when bls does not support the property yet */ || f.isVisible)),
 			userFormats: newFormats.filter(f => !!f.owner) // Always show user's own formats, even if isVisible == false
 		}))
 		.selectpicker('refresh')
@@ -953,7 +953,7 @@ function initNewFormat() {
 		}
 
 		const fileContents = editor.getValue();
-		const fileName = $formatName.val() + '.' + $formatType.selectpicker('val');
+		const fileName = $formatName.val() + '.blf.' + $formatType.selectpicker('val');
 
 		// IE11 does not support File constructor.
 		// var file = new File([new Blob([fileContents])], fileName);

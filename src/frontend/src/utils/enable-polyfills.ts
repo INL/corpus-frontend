b7bd9cf2ed444ab072d73f6b26706310953e651d
Koen Mertens <koen.mertens@ivdnt.org>
2019-03-15 16:03:58 +0100

    Add some missing IE11 polyfills
    Fix SelectPicker vertical-align

--- src/frontend/src/utils/enable-polyfills.ts
+++ src/frontend/src/utils/enable-polyfills.ts
@@ -9,10 +9,43 @@ import 'whatwg-fetch';
 // Except these polyfills, as they are still stage-3 as of writing, and preset-env doesn't support them
 // It also doesn't expand into polyfills for features that haven't been accepted into the ecma standard yet
 import 'core-js/fn/array';
+import 'core-js/web';
 
 // Ironic, our core-js polyfills are behind the ecma standard
 if (typeof Array.prototype.flat !== 'function') {
 	Array.prototype.flat = (Array.prototype as any).flatten;
+}
+
+// from:https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
+([Element.prototype, CharacterData.prototype, DocumentType.prototype])
+.filter(e => !e.hasOwnProperty('remove'))
+.forEach(item => Object.defineProperty(item, 'remove', {
+	configurable: true,
+	enumerable: true,
+	writable: true,
+	value: function remove() {
+		if (this.parentNode !== null) {
+			this.parentNode.removeChild(this);
+		}
+	}
+}));
+
+if (!Element.prototype.matches) {
+	// @ts-ignore
+	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+	Element.prototype.closest = function(s: string) {
+		let el: any = this;
+		do {
+			if (el.matches(s)) {
+				return el;
+			}
+			el = el.parentElement || el.parentNode;
+		} while (el !== null && el.nodeType === 1);
+		return null;
+	};
 }
 
 // DEBUGGING ONLY, this should probably use node.env and be exposed through expose-loader

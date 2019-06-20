750790d6221da54842f7f199ca2326006ca0eb35
Koen Mertens <koen.mertens@ivdnt.org>
2019-06-20 13:58:55 +0200

    Fix descriptions for empty corpora by requiring newer blacklab-server
    Retrieving extended info about empty corpora always throws an error,
    meaning retrieving descriptions for them was impossible.
    Blacklab has been updated to serve descriptions in the basic info, and now we use this instead.
    
    Also fix a bug where description rows could generate invalid HTML ids
    
    Issue #221

--- src/frontend/src/utils/blacklabutils.ts
+++ src/frontend/src/utils/blacklabutils.ts
@@ -210,7 +210,6 @@ export function normalizeIndexOld(id: string, index: BLTypes.BLIndex): Normalize
 		documentFormat: index.documentFormat || null,
 		indexProgress: index.indexProgress || null,
 		tokenCount: index.tokenCount == null ? null : index.tokenCount,
-		description: null,
 	};
 }
 

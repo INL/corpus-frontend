45cc1d1370f2dce0486ed78a3a38c55390f64175
KCMertens <koen.mmm@gmail.com>
2021-04-28 14:14:40 +0200

    (minor) - remove <hr> at top of filters tab

--- src/frontend/src/pages/search/form/QueryFormFilters.vue
+++ src/frontend/src/pages/search/form/QueryFormFilters.vue
@@ -22,7 +22,7 @@
 			>
 				<template v-for="(subtab, j) in tab.subtabs">
 					<h3 v-if="subtab.tabname" :key="j + subtab.tabname">{{subtab.tabname}}</h3>
-					<hr v-else :key="j + subtab.tabname">
+					<hr v-else-if="j !== 0" :key="j + subtab.tabname">
 					<Component v-for="id in subtab.filters" :key="tab.tabname + id"
 						:is="filterMap[id].componentName"
 						:htmlId="i+(j+id) /* brackets or else i+j collapses before stringifying */"

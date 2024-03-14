13149d2fbea46725880a94eb287da7fc9a200bca
KCMertens <koen.mertens@ivdnt.org>
2024-03-14 17:09:46 +0300

    Less polyfills when developing to ease debugging

--- src/frontend/src/utils/vue-shim.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Make typescript pretend that a .vue file always default exports a Vue component
- * This is required because otherwise the compiler complains it can't find the module (because it doesn't/can't parse the file)
- */
-declare module "*.vue" {
-    import Vue from "vue";
-    export default Vue;
-}

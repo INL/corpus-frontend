48ab1f86e2ea27971a899e8040224d67c6323b48
Jan Niestadt <jan.niestadt@ivdnt.org>
2024-04-23 16:02:37 +0200

    MultiValuePicker works.

--- src/frontend/src/components/MultiValuePicker.vue
+++ src/frontend/src/components/MultiValuePicker.vue
@@ -1,10 +1,17 @@
 <template>
 	<div class="multi-value-picker">
 		<SelectPicker :options="options" :value="selectValue" @input="add($event)" data-menu-width="grow" hideEmpty/>
-		<ul v-if="selected.length > 0">
-			<li v-for="v in selected" :key="v">{{ v }}</li>
-		</ul>
-		<p v-else>Select one or more version(s)</p>
+		<div class="selected">
+			<p v-if="selected.length > 0">
+				{{ textSelected || $t('widget.multiValuePicker.textSelected') }}:
+				<ul>
+					<li v-for="v in selected" :key="v.value" :data-value="v.value" title="Click to remove" @click="clickLabel($event?.target)">
+						{{ v.label || v.value }}
+					</li>
+				</ul>
+			</p>
+			<p v-else>{{ textNoneSelected || $t('widget.multiValuePicker.textNoneSelected') }}</p>
+		</div>
 	</div>
 </template>
 
@@ -37,22 +44,65 @@ export default Vue.extend({
 		SelectPicker,
 	},
 	props: {
-		options: { type: Array as () => Option[], default: () => [] as Options },
+		options: {
+			type: Array as () => Option[],
+			default: () => [] as Option[]
+		},
 		value: Array as () => string[]|null,
+		textNoneSelected: {
+			type: String,
+			default: ''
+		},
+		textSelected: {
+			type: String,
+			default: ''
+		},
 	},
 	methods: {
 		add(v: string) {
-			this.selected.push(v);
-			this.$emit('input', this.selected);
-			console.log(this.selected);
-			this.selectValue = null; // TODO: clear selection?
+			const opt = this.options.find(o => (o as Option).value === v);
+			if (opt) {
+				const i = this.selected.indexOf(opt);
+				if (i >= 0) {
+					// Already selected: unselect it
+					this.selected.splice(i, 1);
+				} else {
+					// Not yet selected: add it at the end
+					this.selected.push(opt);
+				}
+			}
+
+			// Clear SelectPicker selection (kinda ugly...)
+			this.selectValue = v;
+			setTimeout(() => {
+				this.selectValue = null;
+			}, 10);
+		},
+		clickLabel(target: EventTarget|null) {
+			if (target && target instanceof HTMLElement) {
+				const v = target.getAttribute('data-value') || target.innerText;
+				this.remove(v);
+			}
+		},
+		remove(v: string) {
+			const opt = this.options.find(o => (o as Option).value === v);
+			if (opt) {
+				const i = this.selected.indexOf(opt);
+				if (i >= 0) {
+					this.selected.splice(i, 1);
+				}
+			}
 		},
 	},
 	data: () =>  ({
-		selected: [] as string[],
+		selected: [] as Option[],
 		selectValue: null as string|null,
 	}),
 	watch: {
+		selected() {
+			console.log('selected:', this.selected);
+			this.$emit('input', this.selected);
+		},
 	},
 	mounted() {
 	},
@@ -61,17 +111,29 @@ export default Vue.extend({
 });
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
-ul {
-	padding: 0;
-	li {
-		list-style-type: none;
-		display: inline-block;
-		background-color: lightblue;
-		padding: 5px;
-		border-radius: 3px;
-		margin: 2px;
+div.selected {
+
+	margin-top: 0.5rem;
+
+	ul {
+		display: inline;
+		padding: 0;
+		li {
+			list-style-type: none;
+			display: inline-block;
+			background-color: lightblue;
+			padding: 5px;
+			border-radius: 3px;
+			margin: 2px;
+			cursor: pointer;
+			&::after {
+				font-weight: bold;
+				content: '✖';
+				margin-left: 5px;
+			}
+		}
 	}
 }
 

af0f5b2b7ab71d2dde98e3ac2f28f73264957c85
Koen Mertens <koen.mertens@ivdnt.org>
2024-07-16 13:36:11 +0200

    Use navbar from component lib. Remove vite styles

--- src/frontend-vue3/src/main.ts
+++ src/frontend-vue3/src/main.ts
@@ -1,18 +1,10 @@
-
-// if (import.meta.env.DEV) {
-// import '@vite/client'
-// }
-
-
-import './assets/main.css'
-import 'vite/modulepreload-polyfill' // usually in builtin index.html, but we disabled that.
+import 'vite/modulepreload-polyfill' // usually imported in index.html, but we don't have one. (it's contained in the java code.)
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
 const app = createApp(App)
 
 app.use(createPinia())

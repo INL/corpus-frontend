fca2210cc104afb762c626bc825bf120f0222215
KCMertens <koen.mertens@ivdnt.org>
2024-08-14 13:13:57 +0200

    Remove unused code

--- src/frontend/src/pages/search/results/table/HitResults.vue
+++ src/frontend/src/pages/search/results/table/HitResults.vue
@@ -6,14 +6,6 @@
 			<slot name="totals"/>
 		</div>
 
-
-		<!-- <ul v-if="colors" class="list-unstyled pull-right text-right">
-			<li v-for="c in colors">
-				{{c.key}}
-				<span class="color-ball" :style="{backgroundColor: c.color}"></span>
-			</li>
-		</ul> -->
-
 		<slot name="groupBy"/>
 		<slot name="pagination"/>
 		<slot name="annotation-switcher"/>
@@ -89,15 +81,6 @@ export default Vue.extend({
 		showTitles: true
 	}),
 	computed: {
-		leftIndex(): number { return this.textDirection === 'ltr' ? 0 : 2; },
-		rightIndex(): number { return this.textDirection === 'ltr' ? 2 : 0; },
-		leftLabel(): string { return this.textDirection === 'ltr' ? 'Before' : 'After'; },
-		rightLabel(): string { return this.textDirection === 'ltr' ? 'After' : 'Before'; },
-		beforeField(): string { return this.textDirection === 'ltr' ? 'left' : 'right'; },
-		afterField(): string { return this.textDirection === 'ltr' ? 'right' : 'left'; },
-		currentPageHitsForGlossStore(): string[] {
-			return this.rows.filter((r): r is HitRowData => r.type === 'hit').map(r => r.hit_id)
-		},
 		colors(): undefined|Record<string, AppTypes.TokenHighlight> {
 			const colors = getHighlightColors(this.results.summary);
 			return Object.keys(colors).length ? colors : undefined;
@@ -133,10 +116,8 @@ export default Vue.extend({
 					});
 				}
 
-				// And display the hit itself
-				if (this.transformSnippets) {
-					this.transformSnippets(hit);
-				}
+				this.transformSnippets?.(hit)
+
 				// ids of the hit, if gloss module is enabled.
 				const {startid, endid} = GlossModule.get.settings()?.get_hit_range_id(hit) ?? {startid: '', endid: ''};
 				const hit_id = GlossModule.get.settings()?.get_hit_id(hit) ?? '';
@@ -158,14 +139,10 @@ export default Vue.extend({
 				return rows;
 			});
 		},
-		numColumns(): number {
-			return 3 + this.shownAnnotationCols.length + this.shownMetadataCols.length + this.shownGlossCols.length; // left - hit - right - (one per shown annotation) - (one per shown metadata)
-		},
 		/** Return all annotations shown in the main search form (provided they have a forward index) */
 		sortableAnnotations(): AppTypes.NormalizedAnnotation[] { return UIStore.getState().results.shared.sortAnnotationIds.map(id => CorpusStore.get.allAnnotationsMap()[id]); },
 		concordanceAnnotationId(): string { return UIStore.getState().results.shared.concordanceAnnotationId; },
 		mainAnnotation(): AppTypes.NormalizedAnnotation { return CorpusStore.get.allAnnotationsMap()[this.concordanceAnnotationId]; },
-		concordanceSize(): number { return UIStore.getState().results.shared.concordanceSize; },
 		concordanceAsHtml(): boolean { return UIStore.getState().results.shared.concordanceAsHtml; },
 		shownAnnotationCols(): AppTypes.NormalizedAnnotation[] {
 			// Don't bother showing the value when we're sorting on the surrounding context and not the hit itself
@@ -193,14 +170,10 @@ export default Vue.extend({
 			// annotations need a forward index to be able to show values (blacklab can't provide them otherwise)
 			return configuredAnnotations.filter(annot => annot.hasForwardIndex);
 		},
-		shownGlossCols(): string[]  {
-			return GlossModule.get.settings()?.gloss_fields.map(f => f.fieldName) ?? []
-		},
 		textDirection: CorpusStore.get.textDirection,
 
 		transformSnippets(): null|((snippet: BLTypes.BLHitSnippet)=> void){ return UIStore.getState().results.shared.transformSnippets; },
 		getDocumentSummary(): ((doc: BLTypes.BLDocInfo, fields: BLTypes.BLDocFields) => any) { return UIStore.getState().results.shared.getDocumentSummary },
-
 	},
 	methods: {
 		changeSort(payload: string) {
